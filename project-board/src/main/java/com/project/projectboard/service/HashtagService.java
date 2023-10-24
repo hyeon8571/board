7fbc14f7376a57130eb637281b87a65502d90e6f
hyeon8571 <w01085914442@gmail.com>
2023-10-24 16:41:25 +0900

    #53 - 해시태그 고도화

--- project-board/src/main/java/com/project/projectboard/service/HashtagService.java
+++ project-board/src/main/java/com/project/projectboard/service/HashtagService.java
@@ -1,19 +1,47 @@
 package com.project.projectboard.service;
 
+import com.project.projectboard.domain.Hashtag;
+import com.project.projectboard.repository.HashtagRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class HashtagService {
-    public Object parseHashtagNames(String content) {
-        return null;
+
+    private final HashtagRepository hashtagRepository;
+
+    public Set<String> parseHashtagNames(String content) {
+        if (content == null) {
+            return Set.of();
+        }
+
+        Pattern pattern = Pattern.compile("#[\\w가-힣]+");
+        Matcher matcher = pattern.matcher(content.strip()); // strip은 앞, 뒤 공백문자 자르기
+        Set<String> result = new HashSet<>();
+
+        while (matcher.find()) { // 정규식에 해당하는 단어가 발견되면
+            result.add(matcher.group().replace("#", ""));
+        }
+
+        return Set.copyOf(result); // result를 불변하게 만들기 위해 Set.copyOf 사용, 그냥 result를 반환해도 결과는 같음
     }
 
-    public Object findHashtagsByNames(Set<String> expectedHashtagNames) {
-        return null;
+    public Set<Hashtag> findHashtagsByNames(Set<String> hashtagNames) {
+        return new HashSet<>(hashtagRepository.findByHashtagNameIn(hashtagNames));
     }
 
-    public void deleteHashtagWithoutArticles(Object any) {
+    public void deleteHashtagWithoutArticles(Long hashtagId) {
+        Hashtag hashtag = hashtagRepository.getReferenceById(hashtagId);
+        if (hashtag.getArticles().isEmpty()) {
+            hashtagRepository.delete(hashtag);
+        }
     }
 }
