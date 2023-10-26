809139f16d6d9840a0d5ba2a5e194364c425ad55
hyeon8571 <w01085914442@gmail.com>
2023-10-26 23:07:00 +0900

    #58 - 대댓글 비즈니스 로직 구현

--- project-board/src/main/java/com/project/projectboard/dto/response/ArticleWithCommentsResponse.java
+++ project-board/src/main/java/com/project/projectboard/dto/response/ArticleWithCommentsResponse.java
@@ -1,12 +1,13 @@
 package com.project.projectboard.dto.response;
 
 
+import com.project.projectboard.dto.ArticleCommentDto;
 import com.project.projectboard.dto.ArticleWithCommentsDto;
 import com.project.projectboard.dto.HashtagDto;
 
 import java.time.LocalDateTime;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public record ArticleWithCommentsResponse(
@@ -43,10 +44,30 @@ public record ArticleWithCommentsResponse(
                 dto.userAccountDto().email(),
                 nickname,
                 dto.userAccountDto().userId(),
-                dto.articleCommentDtos().stream()
-                        .map(ArticleCommentResponse::from)
-                        .collect(Collectors.toCollection(LinkedHashSet::new))
+                organizeChildComments(dto.articleCommentDtos())
         );
     }
 
+    private static Set<ArticleCommentResponse> organizeChildComments(Set<ArticleCommentDto> dtos) {
+        Map<Long, ArticleCommentResponse> map = dtos.stream()
+                .map(ArticleCommentResponse::from)
+                .collect(Collectors.toMap(ArticleCommentResponse::id, Function.identity())); //값이 실제 요소여야 하는 일반적인 경우에서는 두 번째 함수로 Function.identity()를 사용한다.
+
+        map.values().stream()
+                .filter(ArticleCommentResponse::hasParentComment)
+                .forEach(comment -> {
+                    ArticleCommentResponse parentComment = map.get(comment.parentCommentId()); // 부모 댓글이 존재하는, 즉 자식 댓글들을 부모 댓글 안에 저장
+                    parentComment.childComments().add(comment);
+                });
+
+        return map.values().stream()
+                .filter(comment -> !comment.hasParentComment()) // 가장 위 부모 댓글들만 사용
+                .collect(Collectors.toCollection(() ->
+                        new TreeSet<>(Comparator
+                                .comparing(ArticleCommentResponse::createdAt)
+                                .reversed() // 내림차순 정렬
+                                .thenComparingLong(ArticleCommentResponse::id)
+                        )
+                        ));
+    }
 }
